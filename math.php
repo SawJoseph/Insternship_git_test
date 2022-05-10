5a67320edbd2de6070efd33a599dd8e08ebd7178
hninlwin <bcmm.intern06@brycenmyanmar.com.mm>
2022-05-10 15:12:13 +0630

    adding math.php

---
+++ math.php
@@ -0,0 +1,8 @@
+<?php
+
+echo Calculator!;
+
+function add($a,$b){echo $a + $b;}
+function minus($a,$b){echo (a>b) ?$a - $b:$b-$a;}
+function multi($a,$b){echo $a * $b;}
+function divide($a,$b){echo (a>b) ?$a / $b:$b/$a;}
